59ae81765d1606d3c1b8fa5dd4cb7ba61aeb7238
jakebroffman <jake.broffman1088@gmail.com>
2023-02-18 13:53:58 -0500

    edit renderCocktailCard function to elminate a formatting issue with the 'card' the function creates

--- index.js
+++ index.js
@@ -1,110 +1,112 @@
 
 document.addEventListener('DOMContentLoaded', (e) => {    
    
-        const form1 = document.getElementById('nameSearch');
-        const form2 = document.getElementById('liquorSearch');
+    const form1 = document.getElementById('nameSearch');
+    const form2 = document.getElementById('liquorSearch');
         
-        form1.addEventListener('submit', (e) => {
+    form1.addEventListener('submit', (e) => {
             
-            e.preventDefault();
-            document.getElementById('cocktailList').replaceChildren();
-            document.getElementById('ingredientList').replaceChildren();
-            const inputOne = document.querySelector('#searchByName')
+        e.preventDefault();
+        document.getElementById('cocktailList').replaceChildren();
+        document.getElementById('ingredientList').replaceChildren();
+        const inputOne = document.querySelector('#searchByName')
             
-            if (inputOne.value.length > 0) {
+        if (inputOne.value.length > 0) {
             fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputOne.value}`)
             .then(res => res.json())
             .then(cocktailData => cocktailData.drinks.forEach(renderCocktailCard))
             .catch(e => console.error(e))
             }
-            else {
+        else {
 
             }
         })
 
-        form2.addEventListener('submit', (e) => {
+    form2.addEventListener('submit', (e) => {
             
-            e.preventDefault();
+        e.preventDefault();
 
-            document.getElementById('ingredientList').replaceChildren();
-            document.getElementById('cocktailList').replaceChildren();
-            const inputTwo = document.querySelector('#searchByLiquor')
+        document.getElementById('ingredientList').replaceChildren();
+        document.getElementById('cocktailList').replaceChildren();
+        const inputTwo = document.querySelector('#searchByLiquor')
 
-            if (inputTwo.value.length > 0) {
+        if (inputTwo.value.length > 0) {
             fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${inputTwo.value}`)
             .then(res => res.json())
             .then(ingredientData => ingredientData.ingredients.forEach(renderIngredientCard))
             .catch(e => console.error(e))
             }
-            else {
+        else {
 
             }
         })
-    
      
     function renderCocktailCard(cocktailData) {
 
         const div = document.createElement('div')
         const h2 = document.createElement('h2')
         const img = document.createElement('img')
-        const ul = document.createElement('ul')
-        const liOne = document.createElement('li')
-        const liTwo = document.createElement('li')
-        const liThree = document.createElement('li')
-        const liFour = document.createElement('li')
-        const liFive = document.createElement('li')
-        const liSix = document.createElement('li')
-        const liSeven = document.createElement('li')
-        const liEight = document.createElement('li')
-        const liNine = document.createElement('li')
-        const p = document.createElement('p')
+        const h3 = document.createElement('h3')
+        const pOne = document.createElement('p')
+        const pTwo = document.createElement('p')
+        const pThree = document.createElement('p')
+        const pFour = document.createElement('p')
+        const pFive = document.createElement('p')
+        const pSix = document.createElement('p')
+        const pSeven = document.createElement('p')
+        const pEight = document.createElement('p')
+        const pNine = document.createElement('p')
+        const secondH3 = document.createElement('h3')
+        const directions = document.createElement('p')
 
         h2.textContent = cocktailData.strDrink
-        ul.textContent = "Drink Ingredients:"
-        if (cocktailData.strIngredient1 === null) {
-            liOne.innerHTML = ""
+        h3.textContent = "Cocktail Ingredients:"
+        secondH3.textContent = "Directions:"
+
+
+        if (cocktailData.strIngredient1 === null && cocktailData.strMeasure1 === null) {
+            pOne.remove()
         }
-        else { liOne.textContent = `${cocktailData.strMeasure1} ${cocktailData.strIngredient1}`}
+        else { pOne.textContent = `${cocktailData.strMeasure1} ${cocktailData.strIngredient1}`}
         if (cocktailData.strIngredient2 === null) {
-            liTwo.remove()
+            pTwo.remove()
         }
-        else {liTwo.textContent = `${cocktailData.strMeasure2} ${cocktailData.strIngredient2}`}
+        else {pTwo.textContent = `${cocktailData.strMeasure2} ${cocktailData.strIngredient2}`}
         if (cocktailData.strIngredient3 === null) {
-            liThree.remove()
+            pThree.remove()
         }
-        else {liThree.textContent = `${cocktailData.strMeasure3} ${cocktailData.strIngredient3}`}
+        else {pThree.textContent = `${cocktailData.strMeasure3} ${cocktailData.strIngredient3}`}
         if (cocktailData.strIngredient4 === null) { 
-                liFour.remove()
+            pFour.remove()
             }
-        else { liFour.textContent = `${cocktailData.strMeasure4} ${cocktailData.strIngredient4}`}
+        else {pFour.textContent = `${cocktailData.strMeasure4} ${cocktailData.strIngredient4}`}
         if (cocktailData.strIngredient5 === null) { 
-            liFive.remove()
+            pFive.remove()
         }
-        else { liFive.textContent = `${cocktailData.strMeasure5} ${cocktailData.strIngredient5}`}
+        else { pFive.textContent = `${cocktailData.strMeasure5} ${cocktailData.strIngredient5}`}
         if (cocktailData.strIngredient6 === null) { 
-            liSix.remove()
+            pSix.remove()
         }
-        else { liSix.textContent = `${cocktailData.strMeasure6} ${cocktailData.strIngredient6}`}
+        else { pSix.textContent = `${cocktailData.strMeasure6} ${cocktailData.strIngredient6}`}
         if (cocktailData.strIngredient7 === null) {
-            liSeven.remove()
+            pSeven.remove()
         }
-        else { liSeven.textContent = `${cocktailData.strMeasure7} ${cocktailData.strIngredient7}`}
+        else { pSeven.textContent = `${cocktailData.strMeasure7} ${cocktailData.strIngredient7}`}
         if (cocktailData.strIngredient8 === null) {
-            liEight.remove()
+            pEight.remove()
         }
-        else { liEight.textContent = `${cocktailData.strMeasure8} ${cocktailData.strIngredient8}`}
+        else { pEight.textContent = `${cocktailData.strMeasure8} ${cocktailData.strIngredient8}`}
         if (cocktailData.strIngredient9 === null) {
-            liNine.remove()
+            pNine.innerHTML = ""
         }
-        else { liNine.textContent = `${cocktailData.strMeasure9} ${cocktailData.strIngredient9}`}
-        p.textContent = cocktailData.strInstructions
+        else { pNine.textContent = `${cocktailData.strMeasure9} ${cocktailData.strIngredient9}`}
+        directions.textContent = cocktailData.strInstructions
 
         img.src = cocktailData.strDrinkThumb
         img.className = 'cocktail-avatar'
         div.className = 'cocktailCard'
 
-        ul.append(liOne, liTwo, liThree, liFour, liFive, liSix, liSeven, liEight, liNine)
-        div.append(h2, img, ul, p)
+        div.append(h2, img, h3, pOne, pTwo, pThree, pFour, pFive, pSix, pSeven, pEight, pNine, secondH3, p)
         document.querySelector('#cocktailList').append(div)
     }
 
@@ -126,12 +128,12 @@ document.addEventListener('DOMContentLoaded', (e) => {
         document.querySelector('#ingredientList').append(div)
     }
     
-        const clearButton = document.getElementById('clearButton')
+    const clearButton = document.getElementById('clearButton')
 
-        clearButton.addEventListener('click', (e) => {
+    clearButton.addEventListener('click', (e) => {
             
-            document.getElementById('ingredientList').replaceChildren();
-            document.getElementById('cocktailList').replaceChildren();
+        document.getElementById('ingredientList').replaceChildren();
+        document.getElementById('cocktailList').replaceChildren();
 
-        })
     })
+})
